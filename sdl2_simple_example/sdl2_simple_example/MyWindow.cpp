57af2ea8ad7e9393e52e03d3716ba5438bbfbc90
Pau Hernández Vázquez <114944532+PauHeer@users.noreply.github.com>
2024-10-07 13:23:59 +0200

    Pau

--- sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
+++ sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
@@ -1,16 +1,39 @@
+#include <exception>
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_opengl.h>
 #include "MyWindow.h"
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+
 using namespace std;
 
-MyWindow::MyWindow(const std::string& title, int w, int h) : _width(w), _height(h) {
+MyWindow::MyWindow(const char* title, unsigned short width, unsigned short height){
+	open(title, width, height);
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForOpenGL(_window, _ctx);
+    ImGui_ImplOpenGL3_Init("#version 130");
 
+}
+
+MyWindow::~MyWindow() {
+
+    ImGui_ImplSDL2_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
+
+    close();
+}
+
+void MyWindow::open(const char* title, unsigned short width, unsigned short height) {
+    if (isOpen()) return;
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    _window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_OPENGL);
+    _window = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
     if (!_window) throw exception(SDL_GetError());
 
     _ctx = SDL_GL_CreateContext(_window);
@@ -19,12 +42,65 @@ MyWindow::MyWindow(const std::string& title, int w, int h) : _width(w), _height(
     if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
 }
 
-MyWindow::~MyWindow() {
+void MyWindow::close() {
+    if (!isOpen()) return;
+
     SDL_GL_DeleteContext(_ctx);
+    _ctx = nullptr;
+
     SDL_DestroyWindow(static_cast<SDL_Window*>(_window));
+	_window = nullptr;
 }
 
+
+
 void MyWindow::swapBuffers() const {
+    
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("Menu")) {
+            if (ImGui::MenuItem("Adeu")) {
+                // ... codi activació
+                SDL_Event quit_event;
+                quit_event.type = SDL_QUIT;
+                SDL_PushEvent(&quit_event);
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+
     SDL_GL_SwapWindow(static_cast<SDL_Window*>(_window));
+}
+
+bool MyWindow::processEvents(IEventProcessor* event_processor) {
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+		if (event_processor) event_processor->processEvent(e);
+		switch (e.type) {
+		
+        
+        case SDL_QUIT: close();
+            return false;
+
+            break;
+
+
+        default:
+            
+            ImGui_ImplSDL2_ProcessEvent(&e);
+
+            break;
+		}
+    }
+    return true;
 }
 
