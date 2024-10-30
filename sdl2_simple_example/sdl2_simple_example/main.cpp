5f08964db5a2201ad431223562273deeebdf33b8
Pau Hernández Vázquez <114944532+PauHeer@users.noreply.github.com>
2024-10-30 12:13:06 +0100

    s

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -82,17 +82,6 @@ static void drawModel() {
 
 }
 
-//DIBUJAR TRIANGULO
-
-//static void draw_triangle(const u8vec4& color, const vec3& center, double size) {
-//	glColor4ub(color.r, color.g, color.b, color.a);
-//	glBegin(GL_TRIANGLES);
-//	glVertex3d(center.x, center.y + size, center.z);
-//	glVertex3d(center.x - size, center.y - size, center.z);
-//	glVertex3d(center.x + size, center.y - size, center.z);
-//	glEnd();
-//}
-
 // Actualiza la función de visualización para mostrar el modelo en lugar del cubo
 static void display_func() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -115,156 +104,6 @@ static void display_func() {
     drawModel();
 }
 
-
-//DisplayFunct para dibujar CUBO
-
-//static void display_func() {
-//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//
-//    // Configura la matriz de proyección
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    gluPerspective(45.0, (double)WINDOW_SIZE.x / (double)WINDOW_SIZE.y, 0.1, 100.0);
-//
-//    // Configura la matriz de modelo
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    glTranslatef(0.0f, 0.0f, -5.0f); // Mueve la cámara hacia atrás
-//
-//	//draw_triangle(u8vec4(255, 0, 0, 255), vec3(0.0, 0.0, 0.0), 0.5);
-//	
-//	//// Dibuixar una línea 10 unitats amunt
-//	//glLineWidth(2.0f);
-//	//glBegin(GL_LINES);
-//	//glVertex3f(0.f, 0.f, 0.f);
-//	//glVertex3f(0.f, 10.f, 0.f);
-//	//glEnd();
-//
-//    // Aplica la rotación
-//    glRotatef(angle, 1.0f, 1.0f, 0.0f); // Rota alrededor del eje (1, 1, 0)
-//
-//   
-//    //DIBUJA Y PINTA LAS CARAS
-//
-//    glBegin(GL_TRIANGLES);
-//
-//    // Color para la cara frontal
-//    glColor3f(0.0f, 1.0f, 0.0f); // Color verde
-//    // Cara frontal
-//    glVertex3f(-1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//
-//    glVertex3f(-1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, 1.0f);
-//
-//    // Color para la cara trasera
-//    glColor3f(0.0f, 0.0f, 1.0f); // Color azul
-//    // Cara trasera
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(-1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f);
-//
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, -1.0f, -1.0f);
-//
-//    // Color para la cara izquerda
-//    glColor3f(1.0f, 0.5f, 0.0f); // Color naranja
-//    // Cara izquierda
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(-1.0f, -1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, 1.0f);
-//
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(-1.0f, 1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, -1.0f);
-//
-//    // Color para la cara derecha
-//    glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
-//    // Cara derecha
-//    glVertex3f(1.0f, -1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//
-//    glVertex3f(1.0f, -1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, 1.0f);
-//
-//    // Color para la cara superior
-//    glColor3f(1.0f, 1.0f, 1.0f); // Color blanco
-//    // Cara superior
-//    glVertex3f(-1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//
-//    glVertex3f(-1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, 1.0f);
-//
-//    // Color para la cara inferior
-//    glColor3f(1.0f, 1.0f, 0.0f); // Color amarillo
-//    // Cara inferior
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, -1.0f);
-//
-//    glVertex3f(-1.0f, -1.0f, -1.0f);
-//    glVertex3f(-1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, 1.0f);
-//
-//    glEnd();
-//
-//
-//    // DIBUJAR LOS EDGES
-//
-//    glColor3f(0.0f, 0.0f, 0.0f); // Color negro para las aristas
-//    glBegin(GL_LINES);
-//
-//    // Edges frontales
-//    glVertex3f(-1.0f, -1.0f, 1.0f); glVertex3f(1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
-//    glVertex3f(1.0f, 1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
-//
-//    // Edges traseras
-//    glVertex3f(-1.0f, -1.0f, -1.0f); glVertex3f(1.0f, -1.0f, -1.0f);
-//    glVertex3f(1.0f, -1.0f, -1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
-//    glVertex3f(-1.0f, 1.0f, -1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-//
-//    // Conecta los edges frontales y traseros
-//    glVertex3f(-1.0f, -1.0f, -1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, -1.0f, -1.0f); glVertex3f(1.0f, -1.0f, 1.0f);
-//    glVertex3f(1.0f, 1.0f, -1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
-//    glVertex3f(-1.0f, 1.0f, -1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
-//
-//    glEnd();
-//
-//    // Incrementa el ángulo de rotación
-//    angle += 1.0f;
-//}
-
-//Main para el CUBO
-
-//int main1(int argc, char** argv) {
-//	MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
-//
-//	init_openGL();
-//
-//	while(window.processEvents() && window.isOpen()) {
-//		const auto t0 = hrclock::now();
-//		display_func();
-//		window.swapBuffers();
-//		const auto t1 = hrclock::now();
-//		const auto dt = t1 - t0;
-//		if(dt<FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
-//	}
-//
-//	return 0;
-//}
-
 //Main LaserGun
 
 int main(int argc, char** argv) {
@@ -273,7 +112,7 @@ int main(int argc, char** argv) {
     init_openGL();
 
     // Cargar el modelo 3D
-    const char* file = "LaserGun_P1.fbx"; // Ruta del archivo a cargar
+    const char* file = "BakerHouse.fbx"; // Ruta del archivo a cargar
     const struct aiScene* scene = aiImportFile(file, aiProcess_Triangulate | aiProcess_GenNormals);
     if (!scene) {
         fprintf(stderr, "Error en cargar el archivo: %s\n", aiGetErrorString());
