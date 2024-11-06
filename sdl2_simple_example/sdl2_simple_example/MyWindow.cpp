4893090df3a563afa72b88973d4b0e8f1e075148
Pau Hernández Vázquez <114944532+PauHeer@users.noreply.github.com>
2024-11-06 11:15:42 +0100

    texturas medio bien y arrastrar

--- sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
+++ sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
@@ -6,6 +6,10 @@
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 
 using namespace std;
 
@@ -81,25 +85,52 @@ void MyWindow::swapBuffers() const {
     SDL_GL_SwapWindow(static_cast<SDL_Window*>(_window));
 }
 
+bool loadTexture(const char* filename);
+
 bool MyWindow::processEvents(IEventProcessor* event_processor) {
-	SDL_Event e;
-	while (SDL_PollEvent(&e)) {
-		if (event_processor) event_processor->processEvent(e);
-		switch (e.type) {
-		
-        
-        case SDL_QUIT: close();
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        if (event_processor) event_processor->processEvent(e);
+
+        switch (e.type) {
+        case SDL_QUIT:
+            close();
             return false;
 
-            break;
+        case SDL_DROPFILE:
+        {
+            // Obtenemos el archivo soltado
+            const char* dropped_file = e.drop.file;
 
+            // Verificar si es un archivo FBX
+            if (strstr(dropped_file, ".fbx") != nullptr) {
+                // Aquí puedes cargar el archivo FBX usando ASSIMP
+                printf("Archivo FBX soltado: %s\n", dropped_file);
+                const aiScene* new_scene = aiImportFile(dropped_file, aiProcess_Triangulate | aiProcess_GenNormals);
+                if (!new_scene) {
+                    fprintf(stderr, "Error al cargar el archivo FBX: %s\n", aiGetErrorString());
+                }
+                else {
+                    // Procesa el nuevo modelo
+                    // Actualiza tus GameObjects y otras estructuras necesarias
+                }
+            }
+            // Verificar si es un archivo de textura (PNG o DDS)
+            else if (strstr(dropped_file, ".png") != nullptr || strstr(dropped_file, ".dds") != nullptr) {
+                // Cargar la textura usando tu función loadTexture
+                printf("Textura soltada: %s\n", dropped_file);
+                if (!loadTexture(dropped_file)) {
+                    fprintf(stderr, "Error al cargar la textura: %s\n", dropped_file);
+                }
+            }
+            //SDL_free(dropped_file);  //liberar la memoria después de procesar el archivo
+        }
+        break;
 
         default:
-            
             ImGui_ImplSDL2_ProcessEvent(&e);
-
             break;
-		}
+        }
     }
     return true;
 }
--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -8,8 +8,12 @@
 #include <assimp/cimport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-using namespace std;
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
+
+
+using namespace std;
 using hrclock = chrono::high_resolution_clock;
 using u8vec4 = glm::u8vec4;
 using ivec2 = glm::ivec2;
@@ -19,167 +23,166 @@ static const ivec2 WINDOW_SIZE(512, 512);
 static const unsigned int FPS = 60;
 static const auto FRAME_DT = 1.0s / FPS;
 
-const aiScene* scene = nullptr;  // Variable global
+const aiScene* scene = nullptr;
 GLuint textureID;
 
 vector<vec3> vertices;
+vector<glm::vec2> texCoords;  // Vector para almacenar las coordenadas de textura
 vector<unsigned int> indices;
 
-//Variable angulo rotación
+// Variable de ángulo de rotación
 float angle = 0.0f;
 
-#define CHECKERS_HEIGHT 64
-#define CHECKERS_WIDTH 64
+float x = 0.0f;
+float y = 0.0f;
+float z = 0.0f;
 
-GLubyte checkerImage[CHECKERS_HEIGHT][CHECKERS_WIDTH][4];
-
+// Función para cargar la textura desde el archivo "Baker_house.png"
+bool loadTexture(const char* filename) {
+    printf("Cargando textura: %s\n", filename); // Verifica la ruta
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+    if (data) {
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+            nrChannels == 4 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        stbi_image_free(data);
+        return true;
+    }
+    else {
+        fprintf(stderr, "Error al cargar la textura: %s\n", filename);
+        return false;
+    }
+}
 
 static void init_openGL() {
     glewInit();
     if (!GLEW_VERSION_3_0) throw exception("OpenGL 3.0 API is not available.");
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.5, 0.5, 0.5, 1.0);
-    glEnable(GL_TEXTURE_2D);
-    for (int i = 0; i < CHECKERS_HEIGHT; i++) {
-        for (int j = 0; j < CHECKERS_WIDTH; j++) {
-            int c = ((((i & 0x8) == 0) ^ (((j & 0x8)) == 0))) * 255;
-            checkerImage[i][j][0] = (GLubyte)c;
-            checkerImage[i][j][1] = (GLubyte)c;
-            checkerImage[i][j][2] = (GLubyte)c;
-            checkerImage[i][j][3] = (GLubyte)255;
-        }
+
+    // Cargar la textura "Baker_house.png"
+    if (!loadTexture("Baker_house.png")) {
+        printf("No se pudo cargar la textura.");
     }
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKERS_WIDTH, CHECKERS_HEIGHT, 0,
-        GL_RGBA, GL_UNSIGNED_BYTE, checkerImage);
+    else
+    {
+        printf("Textura cargada.");
+    }
 }
 
 // Función para dibujar el modelo cargado
 static void drawModel() {
-    glColor3f(1.0f, 1.0f, 1.0f); // Color blanco
+    glColor3f(1.0f, 1.0f, 1.0f);  // Color blanco para el modelo
+    glBindTexture(GL_TEXTURE_2D, textureID);  // Enlazar la textura
 
     glBegin(GL_TRIANGLES);
     for (size_t i = 0; i < indices.size(); i += 3) {
-        // Primera coordenada de textura y vértice
-        glTexCoord2f(0.0f, 0.0f);  // Coord textura (s, t) para el primer vértice
-        glVertex3d(vertices[indices[i]].x, vertices[indices[i]].y, vertices[indices[i]].z);
+        for (int j = 0; j < 3; ++j) {
+            int vertexIndex = indices[i + j];
+            // Especifica la posición del vértice
+            glVertex3f(vertices[vertexIndex].x, vertices[vertexIndex].y, vertices[vertexIndex].z);
+            // Especifica las coordenadas de textura para el vértice actual
+            glTexCoord2f(texCoords[vertexIndex].x, texCoords[vertexIndex].y);
 
-        // Segunda coordenada de textura y vértice
-        glTexCoord2f(1.0f, 0.0f);  // Coord textura (s, t) para el segundo vértice
-        glVertex3d(vertices[indices[i + 1]].x, vertices[indices[i + 1]].y, vertices[indices[i + 1]].z);
-
-        // Tercera coordenada de textura y vértice
-        glTexCoord2f(0.5f, 1.0f);  // Coord textura (s, t) para el tercer vértice
-        glVertex3d(vertices[indices[i + 2]].x, vertices[indices[i + 2]].y, vertices[indices[i + 2]].z);
+            
+        }
     }
     glEnd();
-
 }
 
-
-float x = 0.0f;
-float y = 0.0f;
-float z = 0.0f;
-// Actualiza la función de visualización para mostrar el modelo en lugar del cubo
 static void display_func() {
+
+    //glEnable(GL_CULL_FACE);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Configura la proyección ortográfica para la vista axonométrica
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);  // Ajusta el área visible en la pantalla
+    glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
 
-    // Configura la matriz de modelo
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluLookAt(x, y, z, 3.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    // Mueve la cámara para alejar la vista axonométrica
-    gluLookAt(x, y, z,   // Aleja la cámara moviéndola más lejos
-        3.0, 0.0, 0.0,   // Punto de enfoque (el centro de la escena)
-        0.0, 1.0, 0.0);  // Vector de arriba (eje Y)
+    glEnable(GL_TEXTURE_2D);  // Habilitar texturas
+    glBindTexture(GL_TEXTURE_2D, textureID);  // Enlazar textura
 
+    drawModel();  // Dibujar el modelo
 
-    /*x += 0.01f;
-    y += 0.01f;
-    z += 0.01f;*/
-
-    // Renderiza el modelo
-    drawModel();
+    glDisable(GL_TEXTURE_2D);  // Desactivar texturas después de dibujar
 }
+
+
+
 void keyboard(unsigned char key, int xMouse, int yMouse) {
     switch (key) {
-    case 'w': // Aumentar X
-        x += 0.1f;
-        break;
-    case 's': // Disminuir X
-        x -= 0.1f;
-        break;
-    case 'a': // Aumentar Z
-        z -= 0.1f;
-        break;
-    case 'd': // Disminuir Z
-        z += 0.1f;
-        break;
-    case 'q': // Aumentar Y
-        y += 0.1f;
-        break;
-    case ' ': // Disminuir Y
-        y -= 0.1f;
-        break;
+    case 'w': x += 0.1f; break;
+    case 's': x -= 0.1f; break;
+    case 'a': z -= 0.1f; break;
+    case 'd': z += 0.1f; break;
+    case 'q': y += 0.1f; break;
+    case ' ': y -= 0.1f; break;
     }
 }
 
-    //Main LaserGun
+int main(int argc, char** argv) {
+    MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
+    init_openGL();
 
-    int main(int argc, char** argv) {
-        // Inicializa la ventana y OpenGL
-        MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
-        init_openGL();
-
-        // Cargar el modelo 3D
-        const char* file = "BakerHouse.fbx"; // Ruta del archivo a cargar
-        const struct aiScene* scene = aiImportFile(file, aiProcess_Triangulate | aiProcess_GenNormals);
-        if (!scene) {
-            fprintf(stderr, "Error en cargar el archivo: %s\n", aiGetErrorString());
-            return -1;
-        }
-
-        // Extraer vértices e índices de la malla cargada
-        for (unsigned int i = 0; i < scene->mNumMeshes; i++) {
-            aiMesh* mesh = scene->mMeshes[i];
-
-            // Extraer los vértices
-            for (unsigned int v = 0; v < mesh->mNumVertices; v++) {
-                aiVector3D vertex = mesh->mVertices[v];
-                vertices.emplace_back(vec3(vertex.x, vertex.y, vertex.z));
-            }
-
-            // Extraer los índices de las caras
-            for (unsigned int f = 0; f < mesh->mNumFaces; f++) {
-                aiFace face = mesh->mFaces[f];
-                for (unsigned int j = 0; j < face.mNumIndices; j++) {
-                    indices.push_back(face.mIndices[j]);
-                }
-            }
-        }
-
-        aiReleaseImport(scene);
-
-        // Bucle principal de renderizado
-        while (window.processEvents() && window.isOpen()) {
-            const auto t0 = hrclock::now();
-            display_func();
-            window.swapBuffers();
-            const auto t1 = hrclock::now();
-            const auto dt = t1 - t0;
-            if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
-        }
-
-        return 0;
+    const char* file = "BakerHouse.fbx";
+    const struct aiScene* scene = aiImportFile(file, aiProcess_Triangulate | aiProcess_GenNormals);
+    if (!scene) {
+        fprintf(stderr, "Error al cargar el archivo: %s\n", aiGetErrorString());
+        return -1;
     }
+
+    // Extraer vértices, índices y coordenadas de textura
+    for (unsigned int i = 0; i < scene->mNumMeshes; i++) {
+        aiMesh* mesh = scene->mMeshes[i];
+
+        // Extraer los vértices y sus coordenadas de textura
+        for (unsigned int v = 0; v < mesh->mNumVertices; v++) {
+            aiVector3D vertex = mesh->mVertices[v];
+            vertices.push_back(vec3(vertex.x, vertex.y, vertex.z));
+
+            // Verifica si el mesh tiene coordenadas de textura (UVs)
+            if (mesh->mTextureCoords[0]) {
+                // Asigna la primera capa de coordenadas de textura (si existen)
+                aiVector3D texCoord = mesh->mTextureCoords[0][v];
+                texCoords.push_back(glm::vec2(texCoord.x, texCoord.y));
+            }
+            else {
+                // Si no tiene coordenadas de textura, agrega coordenadas (0,0)
+                texCoords.push_back(ivec2(0.0f, 0.0f));
+            }
+        }
+
+        // Extraer los índices de las caras
+        for (unsigned int f = 0; f < mesh->mNumFaces; f++) {
+            aiFace face = mesh->mFaces[f];
+            for (unsigned int j = 0; j < face.mNumIndices; j++) {
+                indices.push_back(face.mIndices[j]);
+            }
+        }
+    }
+    aiReleaseImport(scene);
+
+    while (window.processEvents() && window.isOpen()) {
+        const auto t0 = hrclock::now();
+        display_func();
+        window.swapBuffers();
+        const auto t1 = hrclock::now();
+        const auto dt = t1 - t0;
+        if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
+    }
+
+    return 0;
+}
+
